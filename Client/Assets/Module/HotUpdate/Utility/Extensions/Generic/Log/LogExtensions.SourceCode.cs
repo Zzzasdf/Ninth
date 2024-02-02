a7fded04a1078975124bc55f41f1f1317188f5f9
xyr_lishaowei <1449082072@qq.com>
2024-02-02 17:28:49 +0800

    logExtensions modify

--- Client/Assets/Module/HotUpdate/Utility/Extensions/Generic/Log/LogExtensions.SourceCode.cs
+++ Client/Assets/Module/HotUpdate/Utility/Extensions/Generic/Log/LogExtensions.SourceCode.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using UnityEngine;
-using System.Collections.Concurrent;
-using System.Threading;
 
 namespace Ninth.HotUpdate
 {
@@ -13,205 +11,97 @@ namespace Ninth.HotUpdate
     {
         private static class LogSystem
         {
+            private static object lockObj = new object();
             private static LogValidator Validator;
             private static string FramePrefix;
-            private static ConcurrentQueue<LogMessage> logQueue;
-            private static void LogQueueEnqueue(LogValidator validator, string message)
-            {
-                if (!Validator.HasFlag(validator))
-                {
-                    return;
-                }
-                LogMessage logMessage = new LogMessage(validator, message);
-                logQueue.Enqueue(logMessage);
-            }
-
             static LogSystem()
             {
                 Validator = HotUpdateConfig.LogValidator;
                 FramePrefix = string.Join(' ', "Frame".AddColor(ColorDefine.FrameLog), "..");
+            }
 
-                logQueue = new();
-                Thread logThread = new Thread(LogQueueDequeue);
-                logThread.Start();
-
-                void LogQueueDequeue()
+            public static T Debug<T>(LogValidator logValidator, T message, string? format = null)
+            {
+                lock(lockObj)
                 {
-                    while(true)
+                    LogMessage(logValidator, MessageFormat(message, format));
+                    return message;
+                }
+
+                string MessageFormat(T message, string? format = null)
+                {
+                    string result = message?.ToString() ?? string.Empty;
+                    if (!string.IsNullOrEmpty(format))
                     {
-                        Thread.Sleep(1);
-                        if(!logQueue.TryDequeue(out var result) || result == null)
-                        {
-                            continue;    
-                        }
-                        (LogValidator logValidator, string message) = result!;
-                        (Action<string, object[]>? func, Func<string>? format) group = logValidator switch
-                        {
-                            LogValidator.FrameLog => (Debug.LogFormat, FrameFormat),
-                            LogValidator.FrameWarning => (Debug.LogWarningFormat, FrameFormat),
-                            LogValidator.FrameError => (Debug.LogErrorFormat, FrameFormat),
-
-                            LogValidator.Log => (Debug.LogFormat, TimeFormat),
-                            LogValidator.Warning => (Debug.LogWarningFormat, TimeFormat),
-                            LogValidator.Error => (Debug.LogErrorFormat, TimeFormat),
-
-                            _ => (null, null)
-                        };
-                        if (group.func == null || group.format == null)
-                        {
-                            continue;    
-                        }
-                        group.func.Invoke(group.format.Invoke(), new object[] { message });
+                        result = string.Format(format, result);
                     }
+                    return result;
+                }
+            }
 
-                    string FrameFormat()
+            public static IEnumerable<T> Debug<T>(LogValidator logValidator, IEnumerable<T> message, string? format = null)
+            {
+                lock(lockObj)
+                {
+                    LogMessage(logValidator, MessageIEnumerableFormat(message, format));
+                    return message;
+                }
+
+                string MessageIEnumerableFormat<T>(IEnumerable<T> message, string? format = null)
+                {
+                    StringBuilder sb = new StringBuilder().AppendLine();
+                    int index = 0;
+                    foreach (var item in message)
                     {
-                        return FramePrefix + TimeFormat();
+                        sb.AppendLine($" [{index}] => [{item}]");
+                        index++;
                     }
-                    string TimeFormat()
+                    string result = sb.ToString().TrimEnd('\n', '\r');
+                    if (!string.IsNullOrEmpty(format))
+                        result = string.Format(format, result);
+                    return $"{message} => {result}";
+                }
+            }
+
+            private static void LogMessage(LogValidator logValidator, string message)
+            {
+                if(!Validator.HasFlag(logValidator))
+                {
+                    return;
+                }
+                (Action<string, object[]>? func, Func<string>? format) group = logValidator switch
+                {
+                    LogValidator.FrameLog => (UnityEngine.Debug.LogFormat, FrameFormat),
+                    LogValidator.FrameWarning => (UnityEngine.Debug.LogWarningFormat, FrameFormat),
+                    LogValidator.FrameError => (UnityEngine.Debug.LogErrorFormat, FrameFormat),
+
+                    LogValidator.Log => (UnityEngine.Debug.LogFormat, TimeFormat),
+                    LogValidator.Warning => (UnityEngine.Debug.LogWarningFormat, TimeFormat),
+                    LogValidator.Error => (UnityEngine.Debug.LogErrorFormat, TimeFormat),
+
+                    _ => (null, null)
+                };
+                if(group.func == null || group.format == null)
+                {
+                    return;
+                }
+                group.func.Invoke(group.format.Invoke(), new object[] { message });
+
+                string FrameFormat()
+                {
+                    return FramePrefix + TimeFormat();
+                }
+
+                string TimeFormat()
+                {
+                    int currentManagedThreadId = Environment.CurrentManagedThreadId;
+                    string threadIdStr = $"ThreadId: {currentManagedThreadId}";
+                    if(currentManagedThreadId != 1)
                     {
-                        int currentManagedThreadId = Environment.CurrentManagedThreadId;
-                        string threadIdStr = $"ThreadId: {currentManagedThreadId}";
-                        if (currentManagedThreadId != 1)
-                        {
-                            threadIdStr = threadIdStr.AddColor(ColorDefine.NonMainThread);
-                        }
-                        return string.Join(' ', DateTime.Now.ToString(), threadIdStr, "\n{0}");
+                        threadIdStr = threadIdStr.AddColor(ColorDefine.NonMainThread);
                     }
+                    return string.Join(' ', DateTime.Now.ToString(), threadIdStr, "\n{0}");
                 }
-            }
-
-#region Frame
-            public static T FrameLog<T>(T message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.FrameLog, MessageFormat(message, format));
-                return message;
-            } 
-
-            public static IEnumerable<T> FrameLog<T>(IEnumerable<T> message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.FrameLog, MessageIEnumerableFormat(message, format));
-                return message;
-            } 
-
-            public static T FrameWarning<T>(T message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.FrameWarning, MessageFormat(message, format));
-                return message;
-            }
-
-            public static IEnumerable<T> FrameWarning<T>(IEnumerable<T> message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.FrameWarning, MessageIEnumerableFormat(message, format));
-                return message;
-            }
-
-            public static T FrameError<T>(T message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.FrameError, MessageFormat(message, format));
-                return message;
-            }
-
-            public static IEnumerable<T> FrameError<T>(IEnumerable<T> message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.FrameError, MessageIEnumerableFormat(message, format));
-                return message;
-            }
-#endregion
-
-#region Game
-            public static T Log<T>(T message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.Log, MessageFormat(message, format));
-                return message;
-            }
-
-            public static IEnumerable<T> Log<T>(IEnumerable<T> message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.Log, MessageIEnumerableFormat(message, format));
-                return message;
-            }
-
-            public static T Warning<T>(T message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.Warning, MessageFormat(message, format));
-                return message;
-            }
-
-            public static IEnumerable<T> Warning<T>(IEnumerable<T> message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.Warning, MessageIEnumerableFormat(message, format));
-                return message;
-            }
-
-            public static T Error<T>(T message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.Error, MessageFormat(message, format));
-                return message;
-            }
-
-            public static IEnumerable<T> Error<T>(IEnumerable<T> message, string? format)
-            {
-                LogQueueEnqueue(LogValidator.Error, MessageIEnumerableFormat(message, format));
-                return message;
-            }
-#endregion
-
-            private static string MessageFormat<T>(T message, string? format)
-            {
-                string result = message?.ToString() ?? string.Empty;
-                if (!string.IsNullOrEmpty(format))
-                {
-                    result = string.Format(format, result);
-                }
-                return result;
-            }
-
-            private static string MessageIEnumerableFormat<T>(IEnumerable<T> message, string? format)
-            {
-                StringBuilder sb = new StringBuilder().AppendLine();
-                int index = 0;
-                foreach (var item in message)
-                {
-                    sb.AppendLine($" [{index}] => [{item}]");
-                    index++;
-                }
-                string result = sb.ToString().TrimEnd('\n', '\r');
-                if (!string.IsNullOrEmpty(format))
-                    result = string.Format(format, result);
-                return $"{message} => {result}";
-            }
-
-            internal class LogMessage
-            {
-                private LogValidator validator;
-                private string message;
-                public LogMessage(LogValidator validator, string message)
-                {
-                    this.validator = validator;
-                    this.message = message;
-                }
-
-                public void Deconstruct(out LogValidator validator, out string message)
-                {
-                    validator = this.validator;
-                    message = this.message;
-                }
-
-                // public LogValidator validator WholeMessage()
-                // {
-                //     string? format = validator switch
-                //     {
-                //         >= LogValidator.FrameLog and  <= LogValidator.FrameError  => FrameFormat(),
-                //         >= LogValidator.Log and <= LogValidator.Error => TimeFormat(),
-                //         _ => null
-                //     };
-                //     if(format == null)
-                //     {
-                //         return null;
-                //     }
-                //     return string.Format(format, message);
-                // }
             }
         }
     }
--- Client/Assets/Module/HotUpdate/Utility/Extensions/Generic/Log/LogExtensions.cs
+++ Client/Assets/Module/HotUpdate/Utility/Extensions/Generic/Log/LogExtensions.cs
@@ -7,49 +7,48 @@ namespace Ninth.HotUpdate
 {
     public static partial class LogExtensions
     {
+#region Frame
         public static T FrameLog<T>(this T obj, string? format = null)
-            => LogSystem.FrameLog(obj, format);
-
+            => LogSystem.Debug(LogValidator.FrameLog, obj, format);
         public static IEnumerable<T> FrameLog<T>(this IEnumerable<T> obj, string? format = null)
-            => LogSystem.FrameLog(obj, format);
+            => LogSystem.Debug(LogValidator.FrameLog, obj, format);
 
         public static T FrameWarning<T>(this T obj, string? format = null)
-            => LogSystem.FrameWarning(obj, format);
-
+            => LogSystem.Debug(LogValidator.FrameWarning, obj, format);
         public static IEnumerable<T> FrameWarning<T>(this IEnumerable<T> obj, string? format = null)
-            => LogSystem.FrameWarning(obj, format);
+            => LogSystem.Debug(LogValidator.FrameWarning, obj, format);
 
         public static T FrameError<T>(this T obj, string? format = null)
-            => LogSystem.FrameError(obj, format);
-
+            => LogSystem.Debug(LogValidator.FrameError, obj, format);
         public static IEnumerable<T> FrameError<T>(this IEnumerable<T> obj, string? format = null)
-            => LogSystem.FrameError(obj, format);
+            => LogSystem.Debug(LogValidator.FrameError, obj, format);
+#endregion
 
+#region Game
         public static T Log<T>(this T obj, string? format = null)
-            => LogSystem.Log(obj, format);
-
-        public static T[] Log<T>(this T[] obj, string? format = null)
-            => LogSystem.Log<T>(obj, format).ToArray();
-        public static List<T> Log<T>(this List<T> obj, string? format = null)
-            => LogSystem.Log<T>(obj, format).ToList();
-        public static IOrderedEnumerable<T> Log<T>(this IOrderedEnumerable<T> obj, string? format = null)
-            => (IOrderedEnumerable<T>)LogSystem.Log<T>(obj, format);
-        public static ParallelQuery<T> Log<T>(this ParallelQuery<T> obj, string? format = null)
-            => (ParallelQuery<T>)LogSystem.Log<T>(obj, format);
+            => LogSystem.Debug(LogValidator.Log, obj, format);
         public static IEnumerable<T> Log<T>(this IEnumerable<T> obj, string? format = null)
-            => LogSystem.Log(obj, format);
-
+            => LogSystem.Debug(LogValidator.Log, obj, format);
+            
+        public static T[] Log<T>(this T[] obj, string? format = null)
+            => ((obj as IEnumerable<T>).Log(format) as T[])!;
+        public static List<T> Log<T>(this List<T> obj, string? format = null)
+             => ((obj as IEnumerable<T>).Log(format) as List<T>)!;
+        public static IOrderedEnumerable<T> Log<T>(this IOrderedEnumerable<T> obj, string? format = null)
+            => (((obj as IEnumerable<T>)).Log(format) as IOrderedEnumerable<T>)!;
+        public static ParallelQuery<T> Log<T>(this ParallelQuery<T> obj, string? format = null)
+            => (((obj as ParallelQuery<T>)).Log(format) as ParallelQuery<T>)!;
+        
         public static T Warning<T>(this T obj, string? format = null)
-            => LogSystem.Warning(obj, format);
-
+            => LogSystem.Debug(LogValidator.Warning, obj, format);
         public static IEnumerable<T> Warning<T>(this IEnumerable<T> obj, string? format = null)
-            => LogSystem.Warning(obj, format);
+            => LogSystem.Debug(LogValidator.Warning, obj, format);
 
         public static T Error<T>(this T obj, string? format = null)
-            => LogSystem.Error(obj, format);
-
+            => LogSystem.Debug(LogValidator.Error, obj, format);
         public static IEnumerable<T> Error<T>(this IEnumerable<T> obj, string? format = null)
-            => LogSystem.Error(obj, format);
+            => LogSystem.Debug(LogValidator.Error, obj, format);
+#endregion
     }
 }
 
