7de2258743294048c655eb633480f193798ba449
lishaowei <2108528484@qq.com>
2024-03-25 05:05:05 +0800

    ..

--- Client/Assets/HybridCLRData/Generated/AOTGenericReferences.cs
+++ Client/Assets/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -9,86 +9,99 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	//Cysharp.Threading.Tasks.UniTask`1<System.Object>
 	//Cysharp.Threading.Tasks.UniTask`1<System.ValueTuple`2<System.Byte,System.Object>>
 	//Cysharp.Threading.Tasks.UniTask`1<System.ValueTuple`2<System.Object,System.Object>>
-	//Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Object,System.Object>>
 	//Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>
 	//Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Byte,System.Object>>
+	//Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Object,System.Object>>
 	//Ninth.Utility.SubscriberCollect`1<System.Object>
-	//Ninth.Utility.SubscriberCollect`1<System.ValueTuple`2<System.Object,Ninth.HotUpdate.VIEW_HIERARCY>>
-	//Ninth.Utility.SubscriberCollect`2<System.ValueTuple`2<System.Object,Ninth.HotUpdate.VIEW_HIERARCY>,System.Object>
 	//Ninth.Utility.SubscriberCollect`2<System.Object,System.Object>
 	//System.Action`1<System.Object>
+	//System.Action`1<System.Single>
 	//System.Action`1<UnityEngine.InputSystem.InputAction/CallbackContext>
 	//System.Action`1<VContainer.Unity.EntryPointsBuilder>
-	//System.Action`1<System.Single>
-	//System.Collections.Generic.Dictionary`2<System.Object,System.Object>
 	//System.Collections.Generic.Dictionary`2<Ninth.HotUpdate.UIHelperBarMode,System.Object>
+	//System.Collections.Generic.Dictionary`2<System.Object,System.Int32>
 	//System.Collections.Generic.Dictionary`2<System.Object,Ninth.HotUpdate.UIHelperBarMode>
+	//System.Collections.Generic.Dictionary`2<System.Object,System.Object>
 	//System.Collections.Generic.Dictionary`2<System.Int32,System.Object>
 	//System.Collections.Generic.Dictionary`2<System.Int64,System.Object>
-	//System.Collections.Generic.Dictionary`2/Enumerator<System.Object,System.Object>
+	//System.Collections.Generic.Dictionary`2<Ninth.HotUpdate.VIEW_HIERARCHY,System.Int32>
+	//System.Collections.Generic.Dictionary`2<Ninth.HotUpdate.VIEW_HIERARCHY,System.Object>
 	//System.Collections.Generic.Dictionary`2/Enumerator<System.Int32,System.Object>
+	//System.Collections.Generic.Dictionary`2/Enumerator<System.Object,System.Object>
 	//System.Collections.Generic.Dictionary`2/Enumerator<System.Int64,System.Object>
 	//System.Collections.Generic.IEnumerable`1<System.Object>
 	//System.Collections.Generic.KeyValuePair`2<System.Int64,System.Object>
 	//System.Collections.Generic.KeyValuePair`2<System.Int32,System.Object>
 	//System.Collections.Generic.KeyValuePair`2<System.Object,System.Object>
-	//System.Collections.Generic.List`1<Cysharp.Threading.Tasks.UniTask`1<System.Object>>
+	//System.Collections.Generic.LinkedList`1<System.Object>
+	//System.Collections.Generic.LinkedList`1/Enumerator<System.Object>
+	//System.Collections.Generic.LinkedListNode`1<System.Object>
 	//System.Collections.Generic.List`1<System.Int32>
 	//System.Collections.Generic.List`1<System.Object>
 	//System.Collections.Generic.List`1<Ninth.HotUpdate.UIHelperBarMode>
+	//System.Collections.Generic.List`1<Cysharp.Threading.Tasks.UniTask`1<System.Object>>
 	//System.Collections.Generic.List`1/Enumerator<System.Object>
+	//System.Collections.Generic.Queue`1<System.Object>
+	//System.Collections.Generic.SortedDictionary`2<System.Int32,System.Object>
 	//System.Collections.ObjectModel.ReadOnlyCollection`1<Ninth.Utility.Environment>
+	//System.Func`1<Cysharp.Threading.Tasks.UniTaskVoid>
 	//System.Func`1<System.Byte>
-	//System.Func`1<Cysharp.Threading.Tasks.UniTask`1<System.Object>>
-	//System.Func`1<System.Object>
-	//System.Func`2<System.Object,System.Object>
 	//System.Func`2<System.Int32,System.Byte>
 	//System.Func`2<System.Int64,System.Byte>
 	//System.Func`2<System.Object,System.Byte>
-	//System.Nullable`1<Ninth.HotUpdate.VIEW_HIERARCY>
+	//System.Nullable`1<System.ValueTuple`2<System.Nullable`1<System.Int32>,System.Nullable`1<System.Int32>>>
+	//System.Nullable`1<System.Int32>
 	//System.Predicate`1<System.Object>
 	//System.ValueTuple`2<Ninth.HotUpdate.UIHelperBarMode,System.Object>
+	//System.ValueTuple`2<System.Nullable`1<System.Int32>,System.Nullable`1<System.Int32>>
 	//System.ValueTuple`2<System.Byte,System.Object>
-	//System.ValueTuple`2<System.Object,Ninth.HotUpdate.VIEW_HIERARCY>
 	//System.ValueTuple`2<System.Object,System.Object>
 	// }}
 
 	public void RefMethods()
 	{
-		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask/Awaiter,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-UnLoadAllAsync>d__5>(Cysharp.Threading.Tasks.UniTask/Awaiter&,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-UnLoadAllAsync>d__5&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask/Awaiter,Ninth.HotUpdate.StartUp/<StartAsync>d__2>(Cysharp.Threading.Tasks.UniTask/Awaiter&,Ninth.HotUpdate.StartUp/<StartAsync>d__2&)
-		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>,Ninth.HotUpdate.LoginCtrl/<ShowView>d__6>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>&,Ninth.HotUpdate.LoginCtrl/<ShowView>d__6&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>,Ninth.HotUpdate.SettingsCtrl/<ShowView>d__5>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>&,Ninth.HotUpdate.SettingsCtrl/<ShowView>d__5&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>,Ninth.HotUpdate.LoginCtrl/<ShowView>d__4>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>&,Ninth.HotUpdate.LoginCtrl/<ShowView>d__4&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask/Awaiter,Ninth.HotUpdate.SceneProxy/<LoadSceneAsync>d__0>(Cysharp.Threading.Tasks.UniTask/Awaiter&,Ninth.HotUpdate.SceneProxy/<LoadSceneAsync>d__0&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask/Awaiter,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-UnLoadAllAsync>d__5>(Cysharp.Threading.Tasks.UniTask/Awaiter&,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-UnLoadAllAsync>d__5&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UnityAsyncExtensions/AsyncOperationAwaiter,Ninth.HotUpdate.AssetProxyLoadWithAB/<Ninth-HotUpdate-IAssetProxyLoad-UnLoadAllAsync>d__6>(Cysharp.Threading.Tasks.UnityAsyncExtensions/AsyncOperationAwaiter&,Ninth.HotUpdate.AssetProxyLoadWithAB/<Ninth-HotUpdate-IAssetProxyLoad-UnLoadAllAsync>d__6&)
-		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::Start<Ninth.HotUpdate.SceneProxy/<LoadSceneAsync>d__0>(Ninth.HotUpdate.SceneProxy/<LoadSceneAsync>d__0&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::Start<Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-UnLoadAllAsync>d__5>(Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-UnLoadAllAsync>d__5&)
-		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::Start<Ninth.HotUpdate.LoginCtrl/<ShowView>d__6>(Ninth.HotUpdate.LoginCtrl/<ShowView>d__6&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::Start<Ninth.HotUpdate.SettingsCtrl/<ShowView>d__5>(Ninth.HotUpdate.SettingsCtrl/<ShowView>d__5&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::Start<Ninth.HotUpdate.LoginCtrl/<ShowView>d__4>(Ninth.HotUpdate.LoginCtrl/<ShowView>d__4&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::Start<Ninth.HotUpdate.SceneProxy/<LoadSceneAsync>d__0>(Ninth.HotUpdate.SceneProxy/<LoadSceneAsync>d__0&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::Start<Ninth.HotUpdate.StartUp/<StartAsync>d__2>(Ninth.HotUpdate.StartUp/<StartAsync>d__2&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder::Start<Ninth.HotUpdate.AssetProxyLoadWithAB/<Ninth-HotUpdate-IAssetProxyLoad-UnLoadAllAsync>d__6>(Ninth.HotUpdate.AssetProxyLoadWithAB/<Ninth-HotUpdate-IAssetProxyLoad-UnLoadAllAsync>d__6&)
-		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Object,System.Object>>,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__3>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Object,System.Object>>&,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__3&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>,Ninth.HotUpdate.ViewProxy/<GetHierarchy>d__12>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>&,Ninth.HotUpdate.ViewProxy/<GetHierarchy>d__12&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Object,System.Object>>,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__2>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Object,System.Object>>&,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__2&)
-		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>,Ninth.HotUpdate.ViewProxy/<GetHierarchy>d__8>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>&,Ninth.HotUpdate.ViewProxy/<GetHierarchy>d__8&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Object,System.Object>>,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__3>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Object,System.Object>>&,Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__3&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Byte,System.Object>>,Ninth.HotUpdate.DownloaderProxy/<GetRequest>d__0>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.ValueTuple`2<System.Byte,System.Object>>&,Ninth.HotUpdate.DownloaderProxy/<GetRequest>d__0&)
-		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::Start<Ninth.HotUpdate.ViewProxy/<GetHierarchy>d__8>(Ninth.HotUpdate.ViewProxy/<GetHierarchy>d__8&)
-		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::Start<Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__2>(Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__2&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::Start<Ninth.HotUpdate.ViewProxy/<GetHierarchy>d__12>(Ninth.HotUpdate.ViewProxy/<GetHierarchy>d__12&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::Start<Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__3>(Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__3&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::Start<Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__2>(Ninth.HotUpdate.AssetProxy/<Ninth-HotUpdate-IAssetProxy-CloneAsync>d__2&)
 		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskMethodBuilder`1<System.Object>::Start<Ninth.HotUpdate.DownloaderProxy/<GetRequest>d__0>(Ninth.HotUpdate.DownloaderProxy/<GetRequest>d__0&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskVoidMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask/Awaiter,Ninth.HotUpdate.LoginCtrl/<OnBtnSettingsClick>d__7>(Cysharp.Threading.Tasks.UniTask/Awaiter&,Ninth.HotUpdate.LoginCtrl/<OnBtnSettingsClick>d__7&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskVoidMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>,Ninth.HotUpdate.ViewProxy/<RecycleAsync>d__11>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>&,Ninth.HotUpdate.ViewProxy/<RecycleAsync>d__11&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskVoidMethodBuilder::Start<Ninth.HotUpdate.LoginCtrl/<OnBtnSettingsClick>d__7>(Ninth.HotUpdate.LoginCtrl/<OnBtnSettingsClick>d__7&)
+		// System.Void Cysharp.Threading.Tasks.CompilerServices.AsyncUniTaskVoidMethodBuilder::Start<Ninth.HotUpdate.ViewProxy/<RecycleAsync>d__11>(Ninth.HotUpdate.ViewProxy/<RecycleAsync>d__11&)
 		// System.IProgress`1<System.Single> Cysharp.Threading.Tasks.Progress::Create<System.Single>(System.Action`1<System.Single>)
 		// Cysharp.Threading.Tasks.UniTask`1<System.Object[]> Cysharp.Threading.Tasks.UniTask::WhenAll<System.Object>(System.Collections.Generic.IEnumerable`1<Cysharp.Threading.Tasks.UniTask`1<System.Object>>)
 		// System.Object Ninth.Utility.IJsonProxy::ToObject<System.Object>(System.Enum,System.Int32,System.Func`1<System.Object>)
-		// System.Object Ninth.Utility.SubscriberCollect`2<System.Object,System.Object>::Get<Ninth.HotUpdate.VIEW_HIERARCY>(System.Int32)
-		// Ninth.Utility.ReactiveProperty`1<System.ValueTuple`2<System.Object,Ninth.HotUpdate.VIEW_HIERARCY>> Ninth.Utility.SubscriberCollect`2<System.ValueTuple`2<System.Object,Ninth.HotUpdate.VIEW_HIERARCY>,System.Object>::Subscribe<System.Object>(System.ValueTuple`2<System.Object,Ninth.HotUpdate.VIEW_HIERARCY>,System.Int32)
-		// Ninth.Utility.ReactiveProperty`1<System.Object> Ninth.Utility.SubscriberCollect`2<System.Object,System.Object>::Subscribe<Ninth.HotUpdate.VIEW_HIERARCY>(System.Object,System.Int32)
+		// System.Object Ninth.Utility.SubscriberCollect`2<System.Object,System.Object>::Get<Ninth.HotUpdate.VIEW_HIERARCHY>(System.Int32)
+		// Ninth.Utility.ReactiveProperty`1<System.Object> Ninth.Utility.SubscriberCollect`2<System.Object,System.Object>::Subscribe<System.Object>(System.Object,System.Int32)
+		// Ninth.Utility.ReactiveProperty`1<System.Object> Ninth.Utility.SubscriberCollect`2<System.Object,System.Object>::Subscribe<Ninth.HotUpdate.VIEW_HIERARCHY>(System.Object,System.Int32)
 		// System.Object System.Activator::CreateInstance<System.Object>()
+		// System.Boolean System.Collections.Generic.CollectionExtensions::Remove<System.Int32,System.Object>(System.Collections.Generic.IDictionary`2<System.Int32,System.Object>,System.Int32,System.Object&)
+		// System.Int32 System.Linq.Enumerable::First<System.Int32>(System.Collections.Generic.IEnumerable`1<System.Int32>)
 		// System.Collections.Generic.List`1<System.Object> System.Linq.Enumerable::ToList<System.Object>(System.Collections.Generic.IEnumerable`1<System.Object>)
 		// System.Void System.Runtime.CompilerServices.AsyncTaskMethodBuilder::Start<Ninth.HotUpdate.HelperTest/<Update1>d__3>(Ninth.HotUpdate.HelperTest/<Update1>d__3&)
 		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::AwaitUnsafeOnCompleted<System.Runtime.CompilerServices.TaskAwaiter,Ninth.HotUpdate.HelperTest/<Awake>d__2>(System.Runtime.CompilerServices.TaskAwaiter&,Ninth.HotUpdate.HelperTest/<Awake>d__2&)
-		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>,Ninth.HotUpdate.DownloaderProxy/<TestRequest>d__1>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>&,Ninth.HotUpdate.DownloaderProxy/<TestRequest>d__1&)
 		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask/Awaiter,Ninth.HotUpdate.SceneProxy/<TestLoadSceneAsync>d__1>(Cysharp.Threading.Tasks.UniTask/Awaiter&,Ninth.HotUpdate.SceneProxy/<TestLoadSceneAsync>d__1&)
 		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.YieldAwaitable/Awaiter,Ninth.HotUpdate.HelperTest/<Awake>d__2>(Cysharp.Threading.Tasks.YieldAwaitable/Awaiter&,Ninth.HotUpdate.HelperTest/<Awake>d__2&)
+		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::AwaitUnsafeOnCompleted<Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>,Ninth.HotUpdate.DownloaderProxy/<TestRequest>d__1>(Cysharp.Threading.Tasks.UniTask`1/Awaiter<System.Object>&,Ninth.HotUpdate.DownloaderProxy/<TestRequest>d__1&)
+		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::Start<Ninth.HotUpdate.SceneProxy/<TestLoadSceneAsync>d__1>(Ninth.HotUpdate.SceneProxy/<TestLoadSceneAsync>d__1&)
 		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::Start<Ninth.HotUpdate.DownloaderProxy/<TestRequest>d__1>(Ninth.HotUpdate.DownloaderProxy/<TestRequest>d__1&)
 		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::Start<Ninth.HotUpdate.HelperTest/<Awake>d__2>(Ninth.HotUpdate.HelperTest/<Awake>d__2&)
-		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::Start<Ninth.HotUpdate.SceneProxy/<TestLoadSceneAsync>d__1>(Ninth.HotUpdate.SceneProxy/<TestLoadSceneAsync>d__1&)
 		// System.Object System.Threading.Interlocked::CompareExchange<System.Object>(System.Object&,System.Object,System.Object)
 		// System.Object[] UnityEngine.Component::GetComponentsInChildren<System.Object>()
 		// System.Object UnityEngine.GameObject::AddComponent<System.Object>()
@@ -102,10 +115,7 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 		// System.Object UnityEngine.Object::Instantiate<System.Object>(System.Object)
 		// System.Object UnityEngine.Resources::Load<System.Object>(System.String)
 		// VContainer.RegistrationBuilder VContainer.ContainerBuilderExtensions::Register<System.Object>(VContainer.IContainerBuilder,VContainer.Lifetime)
-		// VContainer.RegistrationBuilder VContainer.ContainerBuilderExtensions::RegisterFactory<System.Object>(VContainer.IContainerBuilder,System.Func`2<VContainer.IObjectResolver,System.Func`1<System.Object>>,VContainer.Lifetime)
-		// VContainer.RegistrationBuilder VContainer.ContainerBuilderExtensions::RegisterFactory<Cysharp.Threading.Tasks.UniTask`1<System.Object>>(VContainer.IContainerBuilder,System.Func`2<VContainer.IObjectResolver,System.Func`1<Cysharp.Threading.Tasks.UniTask`1<System.Object>>>,VContainer.Lifetime)
 		// VContainer.RegistrationBuilder VContainer.ContainerBuilderExtensions::RegisterInstance<System.Object>(VContainer.IContainerBuilder,System.Object)
-		// System.Object VContainer.IObjectResolverExtensions::Resolve<System.Object>(VContainer.IObjectResolver)
 		// VContainer.RegistrationBuilder VContainer.RegistrationBuilder::As<System.Object>()
 		// VContainer.RegistrationBuilder VContainer.Unity.EntryPointsBuilder::Add<System.Object>()
 	}
